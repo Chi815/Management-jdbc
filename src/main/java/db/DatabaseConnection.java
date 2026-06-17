f04d3596e15b44315bd8bcd2a2ed563c77796e6d
Chrisha <chrishashre@gmail.com>
2026-06-17 12:55:30 +0545

    Update DatabaseConnection.java

--- src/main/java/db/DatabaseConnection.java
+++ src/main/java/db/DatabaseConnection.java
@@ -6,12 +6,12 @@ import java.sql.DriverManager;
 public class DatabaseConnection {
 
     private static final String URL =
-         "jdbc:postgresql://ep-broad-firefly-ao1zov5j-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require";
+         "jdbc:postgresql:";
     private static final String USER =
-            "neondb_owner";
+            "neondb";
 
     private static final String PASSWORD =
-            "npg_CYD8dQ2bzLxs";
+            "Your_password";
 
     public static Connection getConnection() throws Exception {
 
